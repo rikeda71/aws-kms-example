b0e8537e0fbce6c8c335bc99217b574284c6c0a5
s14t284 <rikeda71@gmail.com>
2022-04-02 18:31:04 +0900

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# aws-kms-example
+
+## Usage
+
+1. create asymmetric key for sign and verify in AWS KMS
+
+2. run following commands
+
+```bash
+export KMS_KEY_ID=*** (key id of created key in AWS KMS)
+npm run exec
+```
---
+++ index.ts
@@ -0,0 +1,64 @@
+import {
+  KMSClient,
+  KMSClientConfig,
+  SignCommand,
+  SignCommandInput,
+  SigningAlgorithmSpec,
+  VerifyCommand,
+  VerifyCommandInput,
+} from '@aws-sdk/client-kms';
+
+/**
+ * 非対称キータイプを利用する想定
+ */
+const keyId = process.env.KMS_KEY_ID;
+const target = 'abcdefghijklmnopqrstuvwxyz';
+// Uint8Array で渡してencode
+const message = new TextEncoder().encode(target);
+const config: KMSClientConfig = {
+  // MEMO: 実際は環境変数などから access_key, secret_access_key, region を設定
+  // credentials: {
+  //   accessKeyId: '',
+  //   secretAccessKey: '',
+  // },
+  // region: 'ap-northeast-1',
+  tls: true,
+  maxAttempts: 2,
+};
+const client = new KMSClient(config);
+const signinput: SignCommandInput = {
+  KeyId: keyId,
+  Message: message,
+  // 4096 byte を超える場合はハッシュダイジェストをMessageに指定、MessageType に DIGEST を指定
+  MessageType: 'RAW',
+  SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PSS_SHA_256,
+};
+
+const scommand = new SignCommand(signinput);
+client
+  .send(scommand)
+  .then((result) => {
+    const signature = new TextDecoder().decode(result.Signature);
+    console.log(signature);
+
+    // verify まで一貫して実施してみる
+    // 実際は分けて実行する
+    const verifyinput: VerifyCommandInput = {
+      KeyId: keyId,
+      Signature: result.Signature,
+      Message: message,
+      SigningAlgorithm: SigningAlgorithmSpec.RSASSA_PSS_SHA_256,
+    };
+    const vcommand = new VerifyCommand(verifyinput);
+    client
+      .send(vcommand)
+      .then((result) => {
+        console.log(`verifyResult: ${result.SignatureValid}`);
+      })
+      .catch((err) => {
+        console.error(`verify error: ${err}`);
+      });
+  })
+  .catch((err) => {
+    console.error(`sign error: ${err}`);
+  });
